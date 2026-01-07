be452891bc43fb63ee4c0a06b2b6acdd1461b74a
Vitalii <vitalii.krilevych.ki.2023@lpnu.ua>
2026-01-07 23:31:28 +0200

    Final clean sync for lab_1

--- lab_2/lab2_14/Entity/UserScore.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace lab2_14.Entity;
-
-public class UserScore
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Score { get; set; }
-}
--- lab_3/lab3_14/api/Models/UserScore.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace lab3_14.api.Models;
-
-public class UserScore
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Score { get; set; }
-}
